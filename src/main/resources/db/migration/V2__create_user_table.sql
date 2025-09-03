46a6d1c6a48be19960240f5227fe966a812e0bb7
jrahman-kainos <jawadur.rahman@kainos.com>
2025-09-03 09:40:54 -0300

    Delete src/main/resources/db/migration/V2__create_user_table.sql

--- src/main/resources/db/migration/V2__create_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE user (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-);
